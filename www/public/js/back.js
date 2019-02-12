7e617e81e1179b00cbe2976b6d2ce500a66b091d
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-02-12 22:34:03 +0100

    add slider

---
+++ www/public/js/back.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+  $('.toggle-nav ').click(function(){
+  
+    $('.sidebar').toggleClass('sidebar--showSidebar');
+    $('.toggle-nav ').toggleClass('toggle');
+  });
+  
+})
