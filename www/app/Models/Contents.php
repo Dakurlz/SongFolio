622e89ba72a3d5d010198941c2eedc99e1cdc22f
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-05-30 14:56:35 +0200

    add replace function to contents

--- www/app/Models/Contents.php
+++ www/app/Models/Contents.php
@@ -101,7 +101,7 @@ class Contents extends BaseSQL
                         "type" => "text",
                         "label" => "Titre",
                         "placeholder" => "Votre titre",
-                        "class" => "input-control col-12 col-lg-4 col-md-4 col-sm-4",
+                        "class" => "input-control target-elment-to-slug col-12 col-lg-5 col-md-5 col-sm-5",
                         "id" => "title",
                         "name" => "title",
                         "required" => true,
@@ -173,7 +173,7 @@ class Contents extends BaseSQL
                     "slug" => [
                         "type" => "slug",
                         "label" => "Lien permanent",
-                        "class" => "",
+                        "class" => "title-value-slug",
                         "presed" => $_SERVER['SERVER_NAME'],
                         "id" => "slug",
                         "name" => "slug",
