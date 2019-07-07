4043fc3ded9124009008d701750589c986fc98f4
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2019-07-07 23:15:01 +0200

    update DB

--- www/app/config/songfolio (9).sql
+++ www/app/config/songfolio (9).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : database
--- Généré le :  jeu. 04 juil. 2019 à 10:12
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.8
+-- Généré le :  Dim 07 juil. 2019 à 20:54
+-- Version du serveur :  5.7.25
+-- Version de PHP :  7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -42,6 +42,14 @@ CREATE TABLE `Albums` (
   `comment_active` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `Albums`
+--
+
+INSERT INTO `Albums` (`id`, `title`, `description`, `slug`, `date_published`, `cover_dir`, `deezer`, `spotify`, `likes`, `category_id`, `comment_active`) VALUES
+(1, '?', 'qsdqsdfsdf', '?', '2019-07-07 12:41:17', 'public/uploads/albums/screeshot7.jpg', '', '', NULL, 1, 1),
+(2, '17 cent', 'qsdqdq', '17-cent', '2019-07-07 16:11:43', 'public/uploads/albums/futurama-fr-memologie-2011.jpg', 'https://www.deezer.com/fr/playlist/3540765226', 'https://open.spotify.com/artist/4cOnXPSZlgiY4NkRws9KPa', NULL, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -54,6 +62,14 @@ CREATE TABLE `Categories` (
   `slug` varchar(255) DEFAULT NULL,
   `type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `Categories`
+--
+
+INSERT INTO `Categories` (`id`, `name`, `slug`, `type`) VALUES
+(1, 'Hip-hop', NULL, 'album'),
+(2, 'Adventure', NULL, 'article');
 
 -- --------------------------------------------------------
 
@@ -94,6 +110,13 @@ CREATE TABLE `Contents` (
   `indexed` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `Contents`
+--
+
+INSERT INTO `Contents` (`id`, `type`, `slug`, `category_id`, `title`, `description`, `content`, `date_create`, `date_edit`, `author`, `img_dir`, `published`, `comment_active`, `indexed`) VALUES
+(1, 'page', 'info', NULL, 'info', 'thththyhytyhth', '<p>tests de my page&nbsp;</p>', '2019-07-07 18:07:16', NULL, 6, 'Format n\'est pas bon.', 1, 0, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -118,6 +141,28 @@ CREATE TABLE `Events` (
   `description` longtext NOT NULL,
   `ticketing` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Likes`
+--
+
+CREATE TABLE `Likes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `type_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `Likes`
+--
+
+INSERT INTO `Likes` (`id`, `user_id`, `type`, `type_id`) VALUES
+(58, 5, 'songs', 22),
+(83, 5, 'songs', 20),
+(93, 6, 'songs', 20);
 
 -- --------------------------------------------------------
 
@@ -189,7 +234,8 @@ CREATE TABLE `Settings` (
 INSERT INTO `Settings` (`id`, `type`, `data`) VALUES
 (1, 'header', '{\"header_menu\": \"1\"}'),
 (2, 'footer', '{\"copyright\": \"Copyright Songfolio 2019\", \"footer_menu\": {\"1\": \"2\", \"2\": \"2\"}, \"footer_menu_nb\": \"2\"}'),
-(4, 'template', '{\"link_color\": \"#a80000\", \"text_color\": \"#555555\", \"title_color\": \"#444444\", \"text_font_name\": \"Lato\", \"title_font_name\": \"Questrial\", \"footer_background\": \"#555555\", \"footer_link_color\": \"#c8c8c8\", \"header_background\": \"#ffffff\", \"header_link_color\": \"#717171\", \"footer_title_color\": \"#808080\"}');
+(4, 'template', '{\"link_color\": \"#a80000\", \"text_color\": \"#555555\", \"title_color\": \"#444444\", \"text_font_name\": \"Lato\", \"title_font_name\": \"Questrial\", \"footer_background\": \"#555555\", \"footer_link_color\": \"#c8c8c8\", \"header_background\": \"#ffffff\", \"header_link_color\": \"#717171\", \"footer_title_color\": \"#808080\"}'),
+(6, 'config', '{\"site_desc\": \"test description \", \"site_name\": \"Mon super site\", \"site_tags\": \"music, album, song\"}');
 
 -- --------------------------------------------------------
 
@@ -212,6 +258,15 @@ CREATE TABLE `Songs` (
   `date_published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `Songs`
+--
+
+INSERT INTO `Songs` (`id`, `name`, `album_id`, `slug`, `likes`, `text`, `description`, `img_dir`, `youtube`, `deezer`, `spotify`, `date_published`) VALUES
+(20, 'Moonlight', NULL, 'moonlight', 0, '<p>fbf</p>', 'my btrain new song', NULL, '', '', '', '2019-07-07 10:37:01'),
+(21, 'Numb', 2, 'numb', 0, '<p>f</p>', 'sqdfqsfds', NULL, '', '', '', '2019-07-07 10:40:02'),
+(22, 'SAD!', 2, 'sad!', 0, '<p>dfsdf</p>', 'sqdfqsdfdqsfd', 'public/uploads/songs/screeshot7.jpg', 'https://www.youtube.com/watch?v=pgN-vvVVxMA', '', '', '2019-07-07 10:57:25');
+
 -- --------------------------------------------------------
 
 --
@@ -233,6 +288,14 @@ CREATE TABLE `Users` (
   `id_facebook` bigint(20) DEFAULT NULL,
   `undeletable` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `Users`
+--
+
+INSERT INTO `Users` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `date_inserted`, `role_id`, `status`, `date_update`, `login_token`, `id_facebook`, `undeletable`) VALUES
+(5, NULL, 'Ivan', 'Naluzhnyi', 'ivan.naluzhnyi@gmail.com', '$2y$10$fOFbj9qsPBd75etkDunsXewLsxSKVCjij67cCSGaKEanMf/yRisXu', '2019-07-07 10:16:29', 1, 0, NULL, '5268b48edde7defcfdb801f149307079', NULL, 1),
+(6, NULL, 'John', 'Doe', 'ivan.naluzhnyi@uppli.fr', '$2y$10$5mEkk07W4ksm0VvpObeOd.A/dCeVqRDLQ4FVHEFDAy9PFPxu7Umj6', '2019-07-07 17:46:12', 1, 0, NULL, 'a9790485a5122b823f18a856d061f319', NULL, 0);
 
 --
 -- Index pour les tables déchargées
@@ -266,6 +329,12 @@ ALTER TABLE `Contents`
 -- Index pour la table `Events`
 --
 ALTER TABLE `Events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `Likes`
+--
+ALTER TABLE `Likes`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -312,13 +381,13 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT pour la table `Albums`
 --
 ALTER TABLE `Albums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `Categories`
 --
 ALTER TABLE `Categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `Comments`
@@ -330,13 +399,19 @@ ALTER TABLE `Comments`
 -- AUTO_INCREMENT pour la table `Contents`
 --
 ALTER TABLE `Contents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `Events`
 --
 ALTER TABLE `Events`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Likes`
+--
+ALTER TABLE `Likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT pour la table `Menus`
@@ -360,25 +435,19 @@ ALTER TABLE `Roles`
 -- AUTO_INCREMENT pour la table `Settings`
 --
 ALTER TABLE `Settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `Songs`
 --
 ALTER TABLE `Songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT pour la table `Songs`
---
-ALTER TABLE `Songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT pour la table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
